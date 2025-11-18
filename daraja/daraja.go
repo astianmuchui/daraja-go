c83673aca4c0ae2dd8b18767aa7c26fbafd0367e
astianmuchui <astianmuchui@gmail.com>
2025-11-18 17:02:59 +0300

    Implemented schemas and function calls for requests and responses

---
+++ daraja/daraja.go
@@ -0,0 +1,305 @@
+package daraja
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"reflect"
+	"time"
+	"net/http"
+	"bytes"
+	"io"
+)
+
+func post [T any] (url string, token string, body []byte, out T) (int, T,[]error) {
+
+	if reflect.TypeOf(out).Kind() != reflect.Struct {
+		return 0, out, []error{}
+	}
+
+	var errs []error
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		return 0, out, []error{err}
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, out, []error{err}
+	}
+	defer resp.Body.Close()
+
+	res, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return resp.StatusCode, out, []error{err}
+	}
+
+	status := resp.StatusCode
+
+	var payload T
+
+	err = json.Unmarshal(res, payload)
+
+	if err != nil {
+		errs = append(errs, err)
+	}
+
+	return status, payload, errs
+}
+
+func get [T any] (url string, token string, out T) (int, T, []error) {
+	if reflect.TypeOf(out).Kind() != reflect.Struct {
+		return 0, out, []error{}
+	}
+
+	var errs []error
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, out, []error{err}
+	}
+
+	if token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{Timeout: 30 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, out, []error{err}
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return resp.StatusCode, out, []error{err}
+	}
+
+
+	err = json.Unmarshal(body, out)
+
+	if err != nil {
+		errs = append(errs, err)
+	}
+
+	return resp.StatusCode, out, errs
+}
+
+func (d *Daraja) Authorize() (bool, []error) {
+	var errs []error
+	var status int
+	var body []byte
+
+	req, err := http.NewRequest("GET", AUTH_URL, nil)
+	req.SetBasicAuth(CONSUMER_KEY, CONSUMER_SECRET)
+
+	if err != nil {
+		errs = append(errs, err)
+	} else {
+		client := &http.Client{Timeout: 30 * time.Second}
+		resp, err := client.Do(req)
+		if err != nil {
+			errs = append(errs, err)
+		} else {
+			defer resp.Body.Close()
+			body, err = io.ReadAll(resp.Body)
+			if err != nil {
+				errs = append(errs, err)
+			}
+			status = resp.StatusCode
+		}
+	}
+
+	resTime := time.Now().UTC()
+
+	if len(errs) > 0 {
+		return false, errs
+	}
+
+	if status != 200 {
+		return false, errs
+	}
+
+	var resp DarajaAuthResponse
+
+	err = json.Unmarshal(body, &resp)
+
+	if err != nil {
+		log.Println("Error Unmarshalling Body")
+		return false, errs
+	}
+
+	d.AccessToken = resp.AccessToken
+	d.Expiry = resTime.Add(time.Duration(resp.ExpiresIn) * time.Second)
+	d.Expiry = d.Expiry.UTC()
+
+	return true, []error{}
+}
+
+/*
+	deternines whether or not the auth token is
+	valid by checking the expiry against the current time
+*/
+
+func (d *Daraja) IsAuthorized() bool {
+	return time.Now().UTC().After(d.Expiry)
+}
+
+func (d *Daraja) RetryAuth(status chan bool, errs chan []error) {
+	go func() {
+		authorized, errors := d.Authorize()
+		status <- authorized
+		errs <- errors
+	}()
+}
+
+func (d *Daraja) B2BPaymentRequest(r *B2BPaymentRequestPayload) (*B2BPaymentResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &B2BPaymentResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(B2B_URL, d.AccessToken, payload, B2BPaymentResponsePayload{})
+
+	if len(errs) > 0 {
+		return &B2BPaymentResponsePayload{}, status, false, errs
+	}
+
+	return &response, status, true, []error{}
+}
+
+func (d *Daraja) ReverseTransaction(r *ReversalRequestPayload) (*QueryTransactionStatusResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &QueryTransactionStatusResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(REVERSAL_URL, d.AccessToken, payload, QueryTransactionStatusResponsePayload{})
+
+	if len(errs) > 0 {
+		return &QueryTransactionStatusResponsePayload{}, status, false, errs
+	}
+
+
+	return &response, status, true, []error{}
+}
+
+func (d *Daraja) QueryTransactionStatus(r *QueryTransactionStatusRequestPayload) (*QueryTransactionStatusResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &QueryTransactionStatusResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(TransactionStatusQuery_URL, d.AccessToken, payload, QueryTransactionStatusResponsePayload{})
+
+	if len(errs) > 0 {
+		return &QueryTransactionStatusResponsePayload{}, status, false, errs
+	}
+
+
+	return &response, status, true, []error{}
+}
+
+func (d *Daraja) B2CPaymentRequest(r *B2CPaymentRequestPayload) (*B2CPaymentResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &B2CPaymentResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(B2CPaymentRequest_URL, d.AccessToken, payload, B2CPaymentResponsePayload{})
+
+	if len(errs) > 0 {
+		return &B2CPaymentResponsePayload{}, status, false, errs
+	}
+
+
+	return &response, status, true, []error{}
+}
+
+func (d *Daraja) LipaNaMpesaOnlinePayment(r *LipaNaMpesaOnlineRequestPayload)  (*LipaNaMpesaOnlinePaymentResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &LipaNaMpesaOnlinePaymentResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(STK_PUSH_URL, d.AccessToken, payload, LipaNaMpesaOnlinePaymentResponsePayload{})
+
+	if len(errs) > 0 {
+		return &LipaNaMpesaOnlinePaymentResponsePayload{}, status, false, errs
+	}
+
+
+	return &response, status, true, []error{}
+}
+
+func (d *Daraja) QueryAccountBalance(r *AccountBalanceQueryRequestPayload) (*AccountBalanceQueryResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &AccountBalanceQueryResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(AccountBalanceQuery_URL, d.AccessToken, payload, AccountBalanceQueryResponsePayload{})
+
+	if len(errs) > 0 {
+		return &AccountBalanceQueryResponsePayload{}, status, false, errs
+	}
+
+
+	return &response, status, true, []error{}
+}
+
+func (d *Daraja) RegisterURLs(r *RegisterURLRequestPayload) (*RegisterURLResponsePayload, int, bool, []error) {
+	if !d.IsAuthorized() {
+		d.Authorize()
+	}
+
+	payload, err := json.Marshal(r)
+
+	if err != nil {
+		return &RegisterURLResponsePayload{}, 0, false, []error{err}
+	}
+
+	status, response, errs := post(AccountBalanceQuery_URL, d.AccessToken, payload, RegisterURLResponsePayload{})
+
+	if len(errs) > 0 {
+		return &RegisterURLResponsePayload{}, status, false, errs
+	}
+
+
+	return &response, status, true, []error{}
+}
---
+++ daraja/schema.go
@@ -0,0 +1,188 @@
+package daraja
+
+import (
+	"time"
+
+)
+
+var (
+	CONSUMER_SECRET = ""
+	CONSUMER_KEY = ""
+	SHORTCODE = ""
+)
+
+const (
+	AUTH_URL                   = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+	C2BConfirmation_URL        = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+	RegisterURL_URL            = "https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl"
+	AccountBalanceQuery_URL    = "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query"
+	STK_PUSH_URL               = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+	REVERSAL_URL               = "https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request"
+	B2B_URL                    = "https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest"
+	TransactionStatusQuery_URL = "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query"
+	OnlineTransactionQuery_URL = "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query"
+	B2CPaymentRequest_URL      = "https://sandbox.safaricom.co.ke/mpesa/b2c/v1/paymentrequest"
+)
+
+type Daraja struct {
+	AccessToken string
+	Expiry      time.Time
+}
+
+type DarajaAuthResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   uint   `json:"expires_in"`
+}
+
+type C2BConfirmationRequestPayload struct {
+	ShortCode     string `json:"ShortCode"`
+	CommandID     string `json:"CommandID"`
+	Amount        string `json:"Amount"`
+	Msidsn        string `json:"Msisdn"`
+	BillRefNumber string `json:"BillRefNumber"`
+}
+
+type B2BPaymentRequestPayload struct {
+	Initiator              string `json:"Initiator"`
+	SecurityCredential     string `json:"SecurityCredential"`
+	CommandID              string `json:"CommandID"`
+	SenderIdentifierType   string `json:"SenderIdentifierType"`
+	RecieverIdentifierType string `json:"RecieverIdentifierType"`
+	Amount                 string `json:"Amount"`
+	PartyA                 string `json:"PartyA"`
+	PartyB                 string `json:"PartyB"`
+	AccountReference       string `json:"AccountReference"`
+	Remarks                string `json:"Remarks"`
+	QueueTimeOutURL        string `json:"QueueTimeOutURL"`
+	ResultURL              string `json:"ResultURL"`
+}
+
+type B2BPaymentResponsePayload struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ConversationID           string `json:"ConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+type RegisterURLRequestPayload struct {
+	ShortCode       string `json:"ShortCode"`
+	ResponseType    string `json:"ResponseType"`
+	ConfirmationURL string `json:"ConfirmationURL"`
+	ValidationURL   string `json:"ValidationURL"`
+}
+
+type RegisterURLResponsePayload struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	OriginatorCoversationID  string `json:"OriginatorCoversationID"` // some responses use a misspelling
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+type AccountBalanceQueryRequestPayload struct {
+	Initiatior         string `json:"Initiator"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	PartyA             string `json:"PartyA"`
+	IdentifierType     string `json:"IdentifierType"`
+	Remarks            string `json:"Remarks"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	ResultURL          string `json:"ResultURL"`
+}
+
+type AccountBalanceQueryResponsePayload struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ConversationID           string `json:"ConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+type LipaNaMpesaOnlineRequestPayload struct {
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	TransactionType   string `json:"TransactionType"`
+	Amount            string `json:"Amount"`
+	PartyA            string `json:"PartyA"`
+	PartyB            string `json:"PartyB"`
+	PhoneNumber       string `json:"PhoneNumber"`
+	CallBackURL       string `json:"CallBackURL"`
+	AccountReference  string `json:"AccountReference"`
+	TransactionDesc   string `json:"TransactionDesc"`
+}
+
+type LipaNaMpesaOnlinePaymentResponsePayload struct {
+	MerchantRequestID   string `json:"MerchantRequestID"`
+	CheckoutRequestID   string `json:"CheckoutRequestID"`
+	ResponseCode        string `json:"ResponseCode"`
+	ResponseDescription string `json:"ResponseDescription"`
+	CustomerMessage     string `json:"CustomerMessage"`
+}
+
+type ReversalRequestPayload struct {
+	Initiator              string `json:"Initiator"`
+	SecurityCredential     string `json:"SecurityCredential"`
+	CommandID              string `json:"CommandID"`
+	TransactionID          string `json:"TransactionID"`
+	Amount                 string `json:"Amount"`
+	ReceiverParty          string `json:"ReceiverParty"`
+	RecieverIdentifierType string `json:"RecieverIdentifierType"`
+	ResultURL              string `json:"ResultURL"`
+	QueueTimeOutURL        string `json:"QueueTimeOutURL"`
+	Remarks                string `json:"Remarks"`
+	Occasion               string `json:"Occasion"`
+}
+
+type ReversalResponsePayload struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ConversationID           string `json:"ConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+type QueryTransactionStatusRequestPayload struct {
+	Initiator          string `json:"Initiator"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	TransactionID      string `json:"TransactionID"`
+	PartyA             string `json:"PartyA"`
+	IdentifierType     string `json:"IdentifierType"`
+	ResultURL          string `json:"ResultURL"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	Remarks            string `json:"Remarks"`
+	Occasion           string `json:"Occasion"`
+}
+
+type QueryTransactionStatusResponsePayload struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ConversationID           string `json:"ConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+type OnlineTransactionQueryPayload struct {
+	BusinessShortCode string `json:"BusinessShortCode"`
+	Password          string `json:"Password"`
+	Timestamp         string `json:"Timestamp"`
+	CheckoutRequestID string `json:"CheckoutRequestID"`
+}
+
+type B2CPaymentRequestPayload struct {
+	InitiatorName      string `json:"InitiatorName"`
+	SecurityCredential string `json:"SecurityCredential"`
+	CommandID          string `json:"CommandID"`
+	Amount             string `json:"Amount"`
+	PartyA             string `json:"PartyA"`
+	PartyB             string `json:"PartyB"`
+	Remarks            string `json:"Remarks"`
+	QueueTimeOutURL    string `json:"QueueTimeOutURL"`
+	ResultURL          string `json:"ResultURL"`
+	Occasion           string `json:"Occasion"`
+}
+
+type B2CPaymentResponsePayload struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ConversationID           string `json:"ConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
